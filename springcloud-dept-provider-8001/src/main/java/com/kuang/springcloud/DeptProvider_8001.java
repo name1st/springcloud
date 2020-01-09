90fc509fa3bef12b739d2dddb358f3ba037e1244
zhaokeyu <2834217423@qq.com>
2020-01-09 23:04:25 +0800

    注册中心

--- springcloud-dept-provider-8001/src/main/java/com/kuang/springcloud/DeptProvider_8001.java
+++ springcloud-dept-provider-8001/src/main/java/com/kuang/springcloud/DeptProvider_8001.java
@@ -2,6 +2,7 @@ package com.kuang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by wonders on 2020-01-08 10:18
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
